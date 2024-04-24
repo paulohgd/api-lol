33b8ebc871ac70b2ee139197f83a226f1d36587a
Paulo Henrique <paulohguerra@outlook.com.br>
2024-04-24 16:47:32 -0300

    Modelo record criado

---
+++ src/main/java/me/apilol/projetospringlol/domain/model/Champions.java
@@ -0,0 +1,12 @@
+package me.apilol.projetospringlol.domain.model;
+
+public record Champions(
+        long id,
+        String name,
+        String role,
+        String lore,
+        String imagemUrl
+
+){
+
+}
